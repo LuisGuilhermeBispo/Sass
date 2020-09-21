3f73c803fa67f24065dd0794fe11663f36f40676
Luis Guilherme Bezerra Bispo <luguilherm@engdb.infra>
2020-09-21 16:15:01 -0300

    Add logo&Banner Parallax

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
   const target = document.querySelector(".target");
   const links = document.querySelectorAll(".mynav a");
-  const colors = ["deepskyblue", "orange", "firebrick", "gold", "magenta", "black", "darkblue"];
+  const colors = ["#ffbf00", "#414151", "firebrick", "gold", "black", "darkblue"];
 
   function mouseenterFunc() {
     if (!this.parentNode.classList.contains("active")) {
